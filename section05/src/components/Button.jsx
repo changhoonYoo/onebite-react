1813188723bf1893e139f9fe5d42abf45a82761e
changhoonYoo <kuilcake@gmail.com>
2025-04-06 15:20:51 +0900

    20250406: 이벤트 처리하기기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,20 @@
 const Button = ({ children, text, color = "black" }) => {
   // { text, color = "black" } 구조분해할당 및 기본값 설정
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    // e: 합성 이벤트 (Synthetic Base Event)
+    // 브라우저 간 이벤트 동작을 일관되게 유지하기 위해
+
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color}
       {children}
     </button>
