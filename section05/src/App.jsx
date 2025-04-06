73b5280e7a4fb404369e21f349d4acafd1090aea
changhoonYoo <kuilcake@gmail.com>
2025-04-06 15:41:33 +0900

    20250406: State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -4,6 +4,8 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Button from "./components/Button";
 
+import { useState } from "react";
+
 function App() {
   // 함수 컴포넌트(대중적): html 태그들을 반환하는 함수
   // 컴포넌트는 대문자로 시작
@@ -18,22 +20,52 @@ function App() {
   //   </>
   //   );
 
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  // const buttonProps = {
+  //   text: "메일",
+  //   color: "red",
+  //   a: 1,
+  //   b: 2,
+  //   c: 3,
+  // };
+
+  // return (
+  //   <>
+  //     <Button {...buttonProps} />
+  //     <Button text={"카페"} />
+  //     <Button text={"블로그"}>
+  //       <Header />
+  //       <div>자식 요소</div>
+  //     </Button>
+  //   </>
+  // );
+
+  // useState(초기값, 상태변화함수)
+  // 컴포넌트 state값이 바뀌면 리랜더링(다시 return) 된다.
+  const [count, setState] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-        <div>자식 요소</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setState(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 }
