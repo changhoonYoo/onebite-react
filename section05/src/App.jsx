2bab9bf317b3d9bf198b8d9e513101e05d952845
changhoonYoo <kuilcake@gmail.com>
2025-04-07 16:58:08 +0900

    20250407: State 와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,8 +3,10 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Button from "./components/Button";
-
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 import { useState } from "react";
+import Register from "./components/Register";
 
 function App() {
   // 함수 컴포넌트(대중적): html 태그들을 반환하는 함수
@@ -41,31 +43,16 @@ function App() {
 
   // useState(초기값, 상태변화함수)
   // 컴포넌트 state값이 바뀌면 리랜더링(다시 return) 된다.
-  const [count, setState] = useState(0);
-  const [light, setLight] = useState("OFF");
+  // const [count, setState] = useState(0);
+  // const [light, setLight] = useState("OFF");
+
+  // 컴포넌트가 리랜더링 되는 3가지 상황
+  // 1. state 값 변경, 2. props 값 변경, 3. 부모 컴포넌트 리랜더링 -> 자식 컴포넌트 리랜더링
+  // state 값은 관련이 없다면 컴포넌트 별로 따로 구분하는 것이 좋다.
 
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setState(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Register />
     </>
   );
 }
