078cbe907e18982afcdb1edd1a6d1412c903dfd3
changhoonYoo <kuilcake@gmail.com>
2025-04-09 18:35:21 +0900

    20250409: 카운터앱 완성

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
 import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>카운터앱</>;
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  // React.js의 데이터 흐름: props를 이용해서 부모에서 자식으로 데이터 전달(단방향 데이터 흐름)
+  return (
+    <>
+      <div className="App">
+        <h1>Simple Counter</h1>
+        <section>
+          <Viewer count={count} />
+        </section>
+        <section>
+          <Controller onClickButton={onClickButton} />
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default App;
