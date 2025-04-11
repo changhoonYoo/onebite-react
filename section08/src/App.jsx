54f93f381d9987c635b322ef44dd228a1ba71211
changhoonYoo <kuilcake@gmail.com>
2025-04-11 18:40:16 +0900

    20250411: TodoList UI setting

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import Header from "./components/Header";
 
 function App() {
-  return <>TodoList</>;
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <Editor />
+        <List />
+      </div>
+    </>
+  );
 }
 
 export default App;
