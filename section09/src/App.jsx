53d5639e22410d7562e1d95b42f4572eceea8ce9
changhoonYoo <kuilcake@gmail.com>
2025-04-15 16:32:04 +0900

    20250415: section09 useReducer

---
+++ section09/src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import Header from "./components/Header";
+import { useState, useRef } from "react";
+import Exam from "./components/exam";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+  // useRef는 컴포넌트가 리렌더링 되어도 값이 유지됨
+  // useRef는 DOM을 직접적으로 조작할 때 사용하기도 하지만, 일반적인 변수 저장에도 사용 가능
+  // useRef는 초기값을 설정할 수 있고, current라는 속성을 통해 값을 변경할 수 있음
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    // targetId를 통해 어떤 id의 todo인지 확인
+    // todos 배열에서 targetId와 같은 id를 가진 todo를 찾아서 isDone 값을 변경
+    setTodos(
+      todos.map((todos) =>
+        todos.id === targetId ? { ...todos, isDone: !todos.isDone } : todos
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    // targetId를 통해 어떤 id의 todo인지 확인
+    // todos 배열에서 targetId와 같은 id를 가진 todo를 찾아서 삭제
+    setTodos(todos.filter((todos) => todos.id !== targetId));
+  };
+
+  return (
+    <>
+      <div className="App">
+        {/* <Header />
+        <Editor onCreate={onCreate} />
+        <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+        <Exam />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ section09/src/components/List.jsx
@@ -0,0 +1,48 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react"; // useState 훅을 사용하여 상태 관리
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState(""); // 검색어 상태 관리
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value); // 입력된 검색어를 상태에 저장
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos; // 검색어가 없으면 전체 todos 반환
+    }
+    return todos.filter((todo) => {
+      return todo.content.toLowerCase().includes(search.toLowerCase()); // 검색어가 포함된 todos 반환
+    });
+  };
+
+  const filteredTodos = getFilteredData(); // 필터링된 todos
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+          // 스프레드 연산자를 사용하여 todo 객체의 모든 속성을 TodoItem 컴포넌트에 전달
+          // key는 React에서 리스트를 렌더링할 때 각 아이템을 구별하기 위해 사용하는 고유한 값
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
