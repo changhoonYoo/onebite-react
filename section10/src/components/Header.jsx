22820233e0e6fa46a72ff48e6e439d09219cce7e
changhoonYoo <kuilcake@gmail.com>
2025-04-15 18:14:33 +0900

    20250415: React.memo와 컴포넌트 렌더링 최적화

--- section10/src/components/Header.jsx
+++ section10/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react";
 
 const Header = () => {
   return (
@@ -9,4 +10,11 @@ const Header = () => {
   );
 };
 
-export default Header;
+// const memoizedHeader = memo(Header);
+// export default memoizedHeader;
+// Header 컴포넌트를 메모이제이션하여 성능 최적화
+// memo는 React.memo()와 동일한 기능을 함
+// memo는 props가 변경되지 않으면 컴포넌트를 다시 렌더링하지 않음
+// memo는 props가 얕은 비교(===)로 변경되었는지 확인함
+
+export default memo(Header);
